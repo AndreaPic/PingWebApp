739a4181e7c516ab1736658427ee1d1f388f4f55
Andrea Piccioni <andrea.piccioni.ap@gmail.com>
2022-04-15 13:05:53 +0200

    Add project files.

---
+++ PingWebApp/Controllers/PingController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Concurrent;
+
+namespace PingWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        private static int callNumber = 0;
+
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILogger _logger;
+
+        public PingController(IHttpClientFactory httpClientFactory, ILogger<PingController> logger)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory; 
+        }
+
+        [HttpGet("Ping")]
+        public async Task<IActionResult> Ping()
+        {
+            Interlocked.Increment(ref callNumber);
+            _logger.LogInformation($"Ping call number: {callNumber}");
+
+            //await Task.Delay(11000);
+            await Task.Delay(1000);
+
+            string ret = string.Empty;
+
+            var httpRequestMessage = new HttpRequestMessage(
+                                HttpMethod.Get,
+                                "https://localhost:7244/api/Pong/Pong");
+
+            var httpClient = _httpClientFactory.CreateClient();
+            httpClient.Timeout = TimeSpan.FromSeconds(10);
+
+            try
+            {
+                var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+                if (httpResponseMessage.IsSuccessStatusCode)
+                {
+                    ret = $"Pong Call Number: {callNumber} - Ping Response: " + await httpResponseMessage.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    ret = $"Call To Ping Return Status Code: {httpResponseMessage.StatusCode}";
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return Ok(ex.Message);
+            }
+
+            return Ok(ret);
+        }
+    }
+}
---
+++ PingWebApp/Program.cs
@@ -0,0 +1,53 @@
+using DistributedLoopDetector;
+using Microsoft.Extensions.Http;
+using PingWebApp;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpContextAccessor().AddLogging().AddHttpClient();
+//Line below activate distributed loop detection
+//builder.Services.AddDistributedLoopDetector(); // <-----
+
+builder.Services.AddControllersWithViews(options =>
+{
+});
+
+builder.Services.AddControllers();
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+//uncomment code below to use distributed cache for loop detection
+/*
+builder.Services.AddDistributedMemoryCache(); // <-----
+builder.Services.AddStackExchangeRedisCache(options => // <-----
+{ 
+    options.Configuration = builder.Configuration.GetConnectionString("MyRedisConStr");
+    options.InstanceName = "redis-dloopd-dev-weu-001";
+});
+*/
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+//uncomment line below to use distributed cache for loop detection
+//app.UseDistributedCacheForLoopDetector("andrea-dev-italy"); // <-----
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.MapRazorPages();
+
+app.Run();
